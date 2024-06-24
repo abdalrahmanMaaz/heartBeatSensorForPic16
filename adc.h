2d5de540d5a0c17e27411a848c7e09152b15642f
abdalrahmanMaaz <aboodfallah78@gmail.com>
2024-06-24 12:21:34 +0300

    Create adc.h

---
+++ adc.h
@@ -0,0 +1,17 @@
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h> 
+
+void delay_adc(int cnt);
+void ADC_Init();
+int ADC_Read(int adcChannel);
+
+#ifdef	__cplusplus
+
+#endif 
+#ifdef	__cplusplus
+}
+#endif 
+
+#endif
